15d8780c488150e6e963d4e2572168dfed5549f1
ypatel2022 <yaxpatel2004@gmail.com>
2024-03-22 21:53:04 -0400

    Setup fetching api requests from backend

--- backend/app.py
+++ backend/app.py
@@ -2,14 +2,24 @@ from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS  
 from os import environ
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)  
 
+
 @app.route('/', methods=['GET'])
+def home():
+  return jsonify({'message': 'The server is running'})
+
 @app.route('/test', methods=['GET'])
 def test():
-  return jsonify({'message': 'The server is running'})
+  # get current time
+  time = datetime.now()
+  current_time = time.strftime("%H:%M:%S")
+
+  return jsonify({'message': '{0} The server is running'.format(current_time)})
+
 
 if __name__ == "__main__":
   app.run(debug=True)
--- wealthwise/app/page.tsx
+++ wealthwise/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
+import { useEffect, useState } from 'react'
 
-export default function Home() {
+export default async function Home() {
+  const res = await fetch('http://localhost:5000/test', { cache: 'no-store' })
+  const data = await res.json()
+
+  console.log(data)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to WealthWise</h1>
-      
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1 className='text-4xl font-bold'>Welcome to WealthWise</h1>
+
+      <h2>{data.message}</h2>
     </main>
-  );
+  )
 }
